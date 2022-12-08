4c75b69d32533d2a6c8a797a551052667cb98d3c
Antoni Seba <antoniwoj@o2.pl>
2022-12-08 15:50:36 +0000

    Make improvments in image recognition functionality

--- image_recognition/urls.py
+++ image_recognition/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('zobacz/<int:image_id>', views.detail, name='detail'),
     path('upload/', views.upload, name='upload'),
+    path("delete/<int:image_id>", views.delete, name='delete'),
+    path('list', views.list, name='list'),
+    path('train', views.train, name='train'),
 ]
